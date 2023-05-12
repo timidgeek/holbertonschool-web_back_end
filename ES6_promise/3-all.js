94230df685565ed7d99b970b296f4244169718a9
timidgeek <9liveslinds@gmail.com>
2023-05-12 02:39:37 +0000

    remove 'error'

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const uploadPromise = uploadPhoto();
+  const createPromise = createUser();
+
+  Promise.all([uploadPromise, createPromise])
+    .then(([uploadResult, createResult]) => {
+      console.log(uploadResult.body, createResult.firstName, createResult.lastName);
+    })
+    .catch(() => {
+      console.log('Signup systen offline');
+    });
+}

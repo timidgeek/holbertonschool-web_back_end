a84248a51619ce892bc917252998574584dd290a
timidgeek <9liveslinds@gmail.com>
2023-05-17 22:52:22 +0000

    can you call it a typo if you type a completely different word?

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -2,7 +2,7 @@ export default class Car {
   constructor(brand, motor, car) {
     this._brand = brand;
     this._motor = motor;
-    this._car = car;
+    this._color = color;
   }
 
   cloneCar() {

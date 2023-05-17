4b69c8686c5fee50445a87f8c9874adb9e8a3b49
timidgeek <9liveslinds@gmail.com>
2023-05-17 03:11:35 +0000

    forgot attribute _ for getter

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -3,20 +3,20 @@ export default class Building {
     // warning message method
     if (this.constructor !== Building
       && typeof this.evacuationWarningMessage !== 'function') {
-      throw Error('Class extending Building must override evacuationWarningMessage');
-    }
+        throw Error('Class extending Building must override evacuationWarningMessage');
+      }
     this._sqft = sqft;
   }
 
   // sqft getter, setter, implementation
   get sqft() {
-    return this.sqft;
+    return this._sqft;
   }
-
+/*
   set sqft(newSqft) {
     if (typeof newSqft !== 'number') {
       throw new TypeError('Sqft must be a number');
     }
     this._sqft = newSqft;
-  }
+  } */
 }

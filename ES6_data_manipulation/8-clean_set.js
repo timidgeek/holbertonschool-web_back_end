a5b75aa147861ffcadf97db7f8106c839b5a3515
timidgeek <9liveslinds@gmail.com>
2023-05-17 23:42:59 +0000

    hoping extra line in output doesn't make a difference hehe

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
+
+  const filteredValues = Array.from(set)
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length));
+
+  return filteredValues.join('-');
+}

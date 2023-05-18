3572baf08ae4128bde72e3e09a9efd4f7f99e94b
timidgeek <9liveslinds@gmail.com>
2023-05-18 01:10:35 +0000

    changed if statement parameters

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,9 @@
 export default function cleanSet(set, startString) {
+  // returns empty string when non-String type is passed in
+  if (typeof startString !== 'string' || startString.lenght < 1 || !startString) {
+    return '';
+  }
+
   const filteredValues = Array.from(set)
     .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length));

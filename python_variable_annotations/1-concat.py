54a081b743935ad9ee1e83037a3fad05695875e9
timidgeek <9liveslinds@gmail.com>
2023-09-06 15:11:43 -0500

    forgot to test before pushing. jumped the gun. removed the quotes around the variables

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -7,4 +7,4 @@ def concat(str1: str, str2: str) -> str:
     type-annotated function that takes two
     strings, and returns a concatenated string
     """
-    return "str1" + "str2"
+    return str1 + str2

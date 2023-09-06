17644f54963bc1a8f513e2b3a09b2850fe53d37e
timidgeek <9liveslinds@gmail.com>
2023-09-06 14:58:53 -0500

    forgot about documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
+
 def add(a: float, b: float) -> float:
+    """
+    type-annotated function that takes two
+    floats, and returns their sum as a float
+    """
     return a + b

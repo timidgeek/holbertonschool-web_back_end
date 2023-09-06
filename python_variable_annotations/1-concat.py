a9ec74befe61ae9daed037c64759e65b1c5c195f
timidgeek <9liveslinds@gmail.com>
2023-09-06 15:09:39 -0500

    got the formula down i believe

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Task 1"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    type-annotated function that takes two
+    strings, and returns a concatenated string
+    """
+    return "str1" + "str2"

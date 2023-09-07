e6d28f658b989e25194adda620a980796c2fbe74
timidgeek <9liveslinds@gmail.com>
2023-09-07 13:28:34 -0500

    string representation

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Task 3"""
+
+
+def to_str(n: float) -> str:
+    """
+    type-annotated function that takes a float
+    as argument and returns it as a string
+    """
+    return str(n)

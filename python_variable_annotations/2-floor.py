a09d641bb67ae57b33b6cc24421300dd13f0a371
timidgeek <9liveslinds@gmail.com>
2023-09-07 13:15:14 -0500

    simpler than we thought, return int to round the float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Task 2"""
+
+
+def floor(n: float) -> int:
+    """
+    type-annotated function that takes a float
+    as argument and returns the floor of the float
+    """
+    return int(n)

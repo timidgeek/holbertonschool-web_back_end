028196755249b240b802d7f37689eee223d59b5a
timidgeek <9liveslinds@gmail.com>
2023-09-07 15:04:24 -0500

    simplify with built in

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Task 5"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    type-annotated function that takes a list of floats
+    as an argument and returns their sum as a float
+    """
+    return sum(input_list)

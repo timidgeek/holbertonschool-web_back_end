9c0e5c2643c8479eb0aa63bc139f8ba4131d8e74
timidgeek <9liveslinds@gmail.com>
2023-09-07 20:27:48 -0500

    reuse task zero

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Task 3"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    regular function that takes an integer and returns asyncio.Task
+    """
+    return asyncio.create_task(wait_random(max_delay))

1b0b3a52e505ac9dd024733608f4e9e6915a6ba5
timidgeek <9liveslinds@gmail.com>
2023-09-07 20:42:14 -0500

    forgot to change parameters for max_delay

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """Task 4"""
 import asyncio
-import random
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     spawn `task_wait_random` n times with the specified `max_delay`
     `task_wait_n` should return the list of all the delays (float values),

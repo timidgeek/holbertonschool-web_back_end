2a9d736d782e7499a19186b661dc0ea8a07e5015
timidgeek <9liveslinds@gmail.com>
2023-09-19 13:48:35 -0500

    task one

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -14,9 +14,11 @@ arguments:
         fields in the log line (message)
 """
 import re
+import logging
 from typing import List
 
 
+# Function Definitions
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """
@@ -26,3 +28,22 @@ def filter_datum(fields: List[str], redaction: str,
     sep = separator
     pattern = f'({sep}|^)({"|".join(map(re.escape, fields))})=([^{sep}]*)'
     return re.sub(pattern, rf'\1\2={redaction}', message)
+
+
+# Class Definitions
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            message, self.SEPARATOR)

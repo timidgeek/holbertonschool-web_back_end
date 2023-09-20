404beeb0b25d7015a9cb15829d727bd10f4c241d
timidgeek <9liveslinds@gmail.com>
2023-09-20 10:54:29 -0500

    pushing for sandbox ease

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """ Module of Index views
 """
-from flask import jsonify, abort
+from flask import jsonify, abort, Blueprint
 from api.v1.views import app_views
 
+
+index = Blueprint('index', __name__)
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
@@ -24,3 +26,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+# Create a new endpoint to raise a 401 error
+@index.route('/api/v1/unauthorized', methods=['GET'])
+def raise_unauthorized_error():
+    abort(401)

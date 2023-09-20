b0021a01470b9191a82adb33e24e1de7413dda52
timidgeek <9liveslinds@gmail.com>
2023-09-20 11:16:21 -0500

    fixed pycodestyle

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views
 
 index = Blueprint('index', __name__)
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status

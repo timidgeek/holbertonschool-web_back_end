e398277d96937ae916f9310721563aa96822b326
timidgeek <9liveslinds@gmail.com>
2023-09-20 16:19:58 -0500

    pycode fix

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -18,7 +18,8 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(
             self, authorization_header: str) -> str:
         """
-        returns the Base64 part of the Authorization header for a Basic Authentication
+        returns the Base64 part of the Authorization
+        header for a Basic Authentication
         """
         if not authorization_header \
                 or not isinstance(authorization_header, str) \

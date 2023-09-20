ba584991c24d1830d7efc7d277a04cb7014062ef
timidgeek <9liveslinds@gmail.com>
2023-09-20 16:18:25 -0500

    task 7

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ class that inherits from Auth
 from flask import request
 from typing import List, TypeVar
 import os
+import re
 import base64
 from api.v1.auth.auth import Auth
 
@@ -19,16 +20,8 @@ class BasicAuth(Auth):
         """
         returns the Base64 part of the Authorization header for a Basic Authentication
         """
-        # Return None if authorization_header is None or not a string
-        if authorization_header is None or not isinstance(
-           authorization_header, str):
+        if not authorization_header \
+                or not isinstance(authorization_header, str) \
+                or not re.search("^Basic ", authorization_header):
             return None
-
-        # Check if authorization_header starts with 'Basic ' (with a space
-        # at the end)
-        if not authorization_header.startswith('Basic '):
-            return None
-
-        # Extract and return the Base64 part after 'Basic '
-        base64_part = authorization_header[len('Basic '):].strip()
-        return base64_part
+        return re.sub("^Basic ", "", authorization_header)

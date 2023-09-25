889b1245a81935df62c71d9430e81ccb481f0760
timidgeek <9liveslinds@gmail.com>
2023-09-25 15:34:27 -0500

    task seven

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """Basic Flask App"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
+
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -12,6 +15,24 @@ def welcome():
 
     # return the message as JSON
     return jsonify(message)
+
+
+@app.route("/users", methods=["POST"])
+def users():
+    try:
+        # get email and password from form data
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        # attempt to register the user using Auth
+        AUTH.register_user(email, password)
+
+        # if successful, return a success message
+        return jsonify({"email": email, "message": "user created"}), 200
+
+    except ValueError as e:
+        # handle the case where the email is already registered
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":

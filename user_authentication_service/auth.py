f1b345425fa67ba95ea73beeed0710c13e80c2b1
timidgeek <9liveslinds@gmail.com>
2023-09-25 13:56:12 -0500

    try/except task 5

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -3,11 +3,34 @@
 user authentication file
 """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        registers users in the database using self._db
+        """
+        # check if user email already exists
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
 
 
 def _hash_password(password: str) -> bytes:
     """
-    password hashed with hashed with `bcrypt.hashpw`
+    password hashed with `bcrypt.hashpw`
     """
     # generate a salt and hash the password
     salt = bcrypt.gensalt()

04d18f5183d9e208239c9fb79799f9f78b622fc4
timidgeek <9liveslinds@gmail.com>
2023-09-26 13:19:50 -0500

    get email through user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -80,14 +80,12 @@ def profile():
     """
     # get session_id
     session_id = request.cookies.get("session_id")
-    # get email
-    email = request.form.get("email")
 
     if session_id:
         # get user
         user = AUTH.get_user_from_session_id(session_id=session_id)
         if user:
-            return jsonify({"email": email}), 200
+            return jsonify({"email": user.email}), 200
     else:
         abort(403)
 

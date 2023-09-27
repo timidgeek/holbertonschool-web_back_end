3c18fb55fda5396385275f72fb4f3f8b60a38330
timidgeek <9liveslinds@gmail.com>
2023-09-26 21:29:53 -0500

    task five, update before_request

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -73,9 +73,11 @@ def before_request():
 
     path_list = ['/api/v1/status/',
                  '/api/v1/unauthorized',
-                 '/api/v1/forbidden']
+                 '/api/v1/forbidden',
+                 '/api/v1/auth_session/login/']
     if auth and auth.require_auth(request.path, path_list):
-        if auth.authorization_header(request) is None:
+        if auth.authorization_header(request) is None \
+            and auth.session_cookie(request) is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)

00a391cac546c9afa6e401939042e47911822dd1
timidgeek <9liveslinds@gmail.com>
2023-09-26 20:16:55 -0500

    task 2 create sess

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -8,14 +8,22 @@ new authentication mechanism that will:
     variables
 
 """
-from typing import TypeVar
-import re
-import base64
 from api.v1.auth.auth import Auth
 from models.user import User
+import uuid
 
 
 class SessionAuth(Auth):
     """
     session auth class that inherits from auth
     """
+    # empty dict class attr
+    user_id_by_session_id = dict()
+
+    def create_session(self, user_id: str = None) -> str:
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        # generate random session_id with uuid4()
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id = {session_id: user_id}
+        return session_id

8c6121cb8936632f2619f20d56be826fbe1a1330
timidgeek <9liveslinds@gmail.com>
2023-10-02 14:10:23 -0500

    initial commit task zero

---
+++ Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+utils unit tests
+"""
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    checks return of method `Nested Map`
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_path, path, expected):
+        """ test nested map access """
+        self.assertEqual(access_nested_map(nested_path, path), expected)

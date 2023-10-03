42cf8d3ab1d8b632168626e1e73788a32b88c683
timidgeek <9liveslinds@gmail.com>
2023-10-03 13:54:33 -0500

    task one, babel setup

---
+++ i18n/1-app.py
@@ -0,0 +1,24 @@
+#!usr/bin/env python3
+""" basic babel setup task one """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config():
+    """ babel config module """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = ["en"]
+    BABEL_DEFAULT_TIMEZONE = ["UTC"]
+
+
+@app.route('/')
+def index():
+    """renders `index.html` doc"""
+    return render_template('0-index.html')
+
+
+if __name__ == 'main':
+    app.run()

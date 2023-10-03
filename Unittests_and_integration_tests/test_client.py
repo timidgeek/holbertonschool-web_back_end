68a47c8c17fd633ddde3cdd53646d4f7f64f84c3
timidgeek <9liveslinds@gmail.com>
2023-10-02 20:42:05 -0500

    task 7

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -52,6 +52,16 @@ class TestGithubOrgClient(unittest.TestCase):
             )
             mock_get_json.assert_called_once()
 
+    @parameterized.expand([
+        ({'license': {'key': 'my_license'}}, 'my_license', True),
+        ({'license': {'key': 'other_license'}}, 'my_license', False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """ test has_license method """
+        self.assertEqual(
+            GithubOrgClient.has_license(repo, license_key), expected
+        )
+
 
 if __name__ == "__main__":
     unittest.main()

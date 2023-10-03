5f83724adc0800cf667da25ffb59cde5ec19d05c
timidgeek <9liveslinds@gmail.com>
2023-10-02 20:19:31 -0500

    task 5

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -23,6 +23,18 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(test_class.org, mock_get_json.return_value)
         mock_get_json.assert_called_once()
 
+    @patch('client.get_json')
+    def test_public_repos_url(self, mock):
+        """ test _public_repos_url method """
+        test_class = GithubOrgClient("google")
+        mock.return_value = {
+            "repos_url": "https://api.github.com/orgs/google/repos"
+        }
+        self.assertEqual(
+            test_class._public_repos_url,
+            mock.return_value.get("repos_url")
+        )
+
 
 if __name__ == "__main__":
     unittest.main()

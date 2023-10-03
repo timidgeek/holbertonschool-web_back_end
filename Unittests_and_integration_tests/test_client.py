5df6aecb5994b6c11e04c1b7a57b97fb3561a0a7
timidgeek <9liveslinds@gmail.com>
2023-10-02 20:32:11 -0500

    public repos task 6

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 GithubOrgClient class tests
 """
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -34,6 +34,23 @@ class TestGithubOrgClient(unittest.TestCase):
             test_class._public_repos_url,
             mock.return_value.get("repos_url")
         )
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """ test public_repos method """
+        mock_get_json.return_value = [{"name": "Test"}]
+        with patch.object(
+            GithubOrgClient,
+            '_public_repos_url',
+            new_callable=PropertyMock
+        ) as mock_public_repos_url:
+            mock_public_repos_url.return_value = "Test"
+            test_class = GithubOrgClient("test")
+            self.assertEqual(
+                test_class.public_repos(),
+                ["Test"]
+            )
+            mock_get_json.assert_called_once()
 
 
 if __name__ == "__main__":

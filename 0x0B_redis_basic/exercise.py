add597ce3d486d4df6cd311527c8d581132b5b65
timidgeek <9liveslinds@gmail.com>
2023-10-13 16:55:40 +0000

    task one?

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Writing strings to Redis"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache():
@@ -14,3 +14,21 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None):
+        if not key:
+            return None
+
+        data = self._redis.get(key)
+        if data is not None and fn is not None:
+            try:
+                return fn(data)
+            except Exception as e:
+                return str(e)
+        return data
+
+    def get_str(self, key: str):
+        return self.get(key, fn=lambda data: data.decode("utf-8"))
+
+    def get_int(self, key: str):
+        return self.get(key, fn=int)

a677aedbdae03a14c498e7cfcc834f4f2f96f18d
timidgeek <9liveslinds@gmail.com>
2023-10-16 14:42:29 +0000

    the ole push and pop - task 3

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -19,12 +19,31 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """stores the history of inputs and outputs"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+    
+        # Append inputs
+        input = str(args)
+        self._redis.rpush(key + ":inputs", input)
+
+        # Append outputs & return
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(key + ":outputs", output)
+        return output
+
+    return wrapper
+
+
 class Cache():
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)

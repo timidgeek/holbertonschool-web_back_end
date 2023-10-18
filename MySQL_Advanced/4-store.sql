a0a04eab3504b96fcbf4bc24b374f3af40d0bcc0
timidgeek <9liveslinds@gmail.com>
2023-10-18 19:01:42 +0000

    familiarized with create trigger

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -1,7 +1,8 @@
 -- Task four
 -- updates inventory of items after orders are made
+CREATE TRIGGER update_inventory
+AFTER INSERT ON orders
+FOR EACH ROW
 UPDATE items
-JOIN orders ON items.name = orders.item_name
-SET items.quantity = items.quantity - orders.number;
-SELECT *
-FROM orders
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name

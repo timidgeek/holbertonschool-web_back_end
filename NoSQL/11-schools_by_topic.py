2ed1dae1102703439ac4aeb97defef4aa33c73b8
timidgeek <9liveslinds@gmail.com>
2023-10-18 17:30:28 -0700

    tasks 9-11

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" task 11 - search list by topic """
+import mongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    function that returns the list of school
+    having a specific topic
+    """
+    schools = mongo_collection.find({'topics': topic})
+    return list(schools)

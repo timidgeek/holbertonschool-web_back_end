69852c4a0ceb6b1a135cee9d8e2a0a5b311e87cb
timidgeek <9liveslinds@gmail.com>
2023-10-24 20:38:45 +0000

    had to use event listeners, didn't work the way i wanted but it's ok

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,17 +1,13 @@
 // task one - recieve input from cmd line
-const readline = require('readline');
-
-// create interface for interacting w/ cmd line
-const cli = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+console.log('Welcome to Holberton School, what is your name?');
+// user input event listener
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+  if (input) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
 });
-
-// prompt for user input
-cli.question('Welcome to Holberton School, what is your name?' + '\n', (name) => {
-  // process user input
-  console.log(`Your name is: ${name}`);
-  // add an exit message when closing the readline interface
-  cli.close();
+// exit event listener
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });

73b15bf10fc903939c51e5ff283d05a94fe1acf7
timidgeek <9liveslinds@gmail.com>
2023-10-29 04:38:33 +0000

    update parsing, fields

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -10,34 +10,33 @@ module.exports = function countStudents(path) {
     // read database file synchronously
     const db = fs.readFileSync(path, 'utf8')
     // create list for students
-    const students = [];
+    // const students = [];
 
     // parse the CSV data
-    db.trim().split('\n').forEach((line) => {
-      const data = line.split(',');
-      if (data.length === 4) {
-        const [firstName, lastName, age, field] = data;
-        students.push({ firstName, lastName, age, field });
-      }
-    });
+    let data = db.split('\n');
+    data = data.filter((line) => line !== '').slice(1);
   
     // if database is available, log numStudents
-    console.log(`Number of students: ${students.length}`);
+    console.log(`Number of students: ${data.length}`);
 
     // log number of students in each field in special format
     const fields = {};
-    students.forEach((student) => {
-      if (fields[student.field]) {
-        fields[student.field].count++;
-        fields[student.field].list.push(student.firstName);
-      } else {
-        fields[student.field] = { count: 1, list: [student.firstName] };
+    for (const line of data) {
+      if (line) {
+        const student = line.split(',');
+        if (!fields[student[3]]) {
+          fields[student[3]] = [];
+        }
+        fields[student[3]].push(student[0]);
       }
-    });
+    }
 
     for (const field in fields) {
-      console.log(`Number of students in ${field}: ${fields[field].count}. List: ${fields[field].list.join(', ')}`);
+      if (field) {
+        console.log(`Number of students in ${field}: ${fields[field].count}. List: ${fields[field].list.join(', ')}`);
+      }
     }
+  
   } catch (err) {
     console.error('Error: Cannot load the database');
   }

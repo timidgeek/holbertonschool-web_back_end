7878de3b709768db6790b862b8c42fbdf6853a23
timidgeek <9liveslinds@gmail.com>
2023-11-02 22:03:45 +0000

    task two

---
+++ queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,26 @@
+// task two - my little redis server
+import redis from 'redis';
+import { createClient } from 'redis';
+const client = createClient();
+
+// log connection failure message
+client.on('error', err => console.log('Redis client not connected to the server: ERROR_MESSAGE', err));
+
+// log connection success message
+client.on('connect', () => console.log('Redis client connected to the server'));
+
+function setNewSchool(schoolName, value){
+  // set key schoolName to value
+  // display a confirmation message using redis.print
+  client.set(schoolName, value, redis.print)
+};
+
+function displaySchoolValue(schoolName){
+  client.get(schoolName, (err, value) => {
+    console.log(value)
+  })
+};
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
